bf9553af6e19e7a1f1692cbb01966ea77bc667c7
JackCurtis <jack.v.curtis@gmail.com>
2015-01-05 18:29:14 -0500

    added route for new selfy

--- app/controllers/selfies_controller.rb
+++ app/controllers/selfies_controller.rb
@@ -2,4 +2,20 @@ class SelfiesController < ApplicationController
   def index
     @selfies = Selfy.all
   end
+
+  def show
+    @selfy = Selfy.find(params[:id])
+  end
+
+  def new
+    @selfy = Selfy.new
+  end
+
+
+  private
+
+  def selfy_params
+    params.require(:selfy).permit(:body)
+  end
+
 end
